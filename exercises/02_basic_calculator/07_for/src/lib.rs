1673b779995c335ddd744b1025639bef19562e34
rishav76dev <rjrishav037@gmail.com>
2025-05-10 16:37:49 +0000

    basic cal done

--- exercises/02_basic_calculator/07_for/src/lib.rs
+++ exercises/02_basic_calculator/07_for/src/lib.rs
@@ -1,6 +1,13 @@
 // Rewrite the factorial function using a `for` loop.
 pub fn factorial(n: u32) -> u32 {
-    todo!()
+   
+    let mut result=1;
+    
+    for i in 2..=n {
+        result *= i;
+    }
+
+    return result;
 }
 
 #[cfg(test)]
